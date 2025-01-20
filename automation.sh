ec9e391f1f3bf1775bb41dad2b9ebc1074fba486
anxgzmcrnld <howard115@gmail.com>
2025-01-20 15:33:04 +0000

    Add automation script for generating and committing new daily learning materials

---
+++ automation.sh
@@ -0,0 +1,11 @@
+cd /root/projects/English-Learning-Material-Generator/
+source .venv/bin/activate
+set -o allexport; source .env; set +o allexport
+
+python3 main.py
+
+last_day=$(ls demo | grep -o 'day[0-9]\+' | sort -V | tail -n 1 | sed 's/day//'); new_day=$((last_day + 1)); cp Exquisite_handout.md demo/day$new_day.md
+
+git add .
+git commit -m "update $(ls demo | grep -o 'day[0-9]\+' | sort -V | tail -n 1).md"
+git push
